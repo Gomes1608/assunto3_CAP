e7167bb8067409b0730f10928bfba6f9779f43d4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-27 11:03:15 -0300

    finalização assunto 3

--- src/exercicio5/Consultorio.java
+++ src/exercicio5/Consultorio.java
@@ -1,40 +1,42 @@
 package exercicio5;
 
 import filas.FilaInt;
+import filas.FilaString;
 
 import java.util.Scanner;
 
 public class Consultorio {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        FilaInt fila = new FilaInt();
+        FilaString fila = new FilaString();
         fila.init();
         int opc;
-        int rm;
         do {
-            System.out.println("1 - Insere aluno na fila");
-            System.out.println("2 - Atende aluno");
+            System.out.println("1 - Insere paciente na fila");
+            System.out.println("2 - Atende paciente");
             System.out.println("3 - Encerra atendimento");
             opc = sc.nextInt();
             switch (opc){
                 case 1:
-                    System.out.println("Digite o rm do aluno: ");
-                    rm = sc.nextInt();
-                    fila.enqueue(rm);
+                    System.out.println("Digite o nome do Paciente: ");
+                    sc.nextLine();
+                    String nome = sc.nextLine();
+                    fila.enqueue(nome);
                     break;
                 case 2:
                     if (fila.isEmpty()){
-                        System.out.println("Não tem alunos na fila");
+                        System.out.println("Não tem pacientes na fila");
                     }else {
-                        System.out.println("Aluno: "+fila.dequeue()+" sera atendido agora");
+                        System.out.println("Paciente: "+fila.dequeue()+" sera atendido agora");
                     }
                     break;
                 case 3:
                     if (!fila.isEmpty()){
-                        System.out.println("Ainda existem alunos na fila");
+                        System.out.println("Ainda existem pacientes na fila");
                         opc = 0;
-                    }
+                    }else {
                     System.out.println("Atendimento encerrado");
+                    }
                     break;
                 default:
                     System.out.println("Opção inválida");
